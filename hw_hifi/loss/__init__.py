db0e5dcebb706f3c40d53231fab485361cafdad6
mcnckc <s20b1_gusev@179.ru>
2024-01-13 17:34:42 +0300

    clean files

--- hw_hifi/loss/__init__.py
+++ hw_hifi/loss/__init__.py
@@ -1,5 +1,2 @@
-from hw_hifi.loss.CTCLossWrapper import CTCLossWrapper as CTCLoss
-
 __all__ = [
-    "CTCLoss"
 ]
--- hw_hifi/metric/__init__.py
+++ hw_hifi/metric/__init__.py
@@ -1,15 +1,3 @@
-from hw_hifi.metric.cer_metric import ArgmaxCERMetric
-from hw_hifi.metric.wer_metric import ArgmaxWERMetric
-from hw_hifi.metric.bs_wer_metric import BeamSearchWERMetric
-from hw_hifi.metric.bs_cer_metric import BeamSearchCERMetric
-from hw_hifi.metric.pyctc_bs_wer_metric import PyCTCBeamSearchWERMetric
-from hw_hifi.metric.pyctc_bs_cer_metric import PyCTCBeamSearchCERMetric
 
 __all__ = [
-    "ArgmaxWERMetric",
-    "ArgmaxCERMetric",
-    "BeamSearchWERMetric",
-    "BeamSearchCERMetric",
-    "PyCTCBeamSearchWERMetric",
-    "PyCTCBeamSearchCERMetric"
 ]
