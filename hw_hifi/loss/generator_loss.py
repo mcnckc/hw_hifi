b901aacf63b03b61c409c16f97af6eccdf640abd
mcnckc <s20b1_gusev@179.ru>
2024-01-18 22:59:33 +0300

    no debug

--- hw_hifi/loss/generator_loss.py
+++ hw_hifi/loss/generator_loss.py
@@ -5,10 +5,9 @@ from torch import nn
 class GeneratorLoss(nn.Module):
     def forward(self, disc_outs,
                 **batch) -> Tensor:
-        print("Calculate generator loss")
         loss = 0
         for out in disc_outs:
             loss += torch.mean((1 - out) ** 2)
-            print("DISCR CUR LOSS", torch.max(out).item(), torch.min(out).item(), torch.median(out).item(), torch.mean(out).item())
+            #print("DISCR CUR LOSS", torch.max(out).item(), torch.min(out).item(), torch.median(out).item(), torch.mean(out).item())
         return loss
 
