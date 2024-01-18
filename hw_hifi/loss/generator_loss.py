941b9c1a18a5ab495d4e1e537766c9951b1c590a
mcnckc <s20b1_gusev@179.ru>
2024-01-18 19:46:15 +0300

    debu

--- hw_hifi/loss/generator_loss.py
+++ hw_hifi/loss/generator_loss.py
@@ -9,6 +9,6 @@ class GeneratorLoss(nn.Module):
         loss = 0
         for out in disc_outs:
             loss += torch.mean((1 - out) ** 2)
-            print("DISCR CUR LOSS", torch.max(out), torch.min(out), torch.median(out), torch.mean(out))
+            print("S", torch.max(out), torch.min(out))
         return loss
 
