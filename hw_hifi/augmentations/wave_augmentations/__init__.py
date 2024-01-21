17021d68756e9e7104cc76863ae61800391b70b2
mcnckc <s20b1_gusev@179.ru>
2024-01-22 01:44:50 +0300

    clear

--- hw_hifi/augmentations/wave_augmentations/__init__.py
+++
@@ -1,5 +0,0 @@
-from hw_hifi.augmentations.wave_augmentations.Gain import Gain
-
-__all__ = [
-    "Gain"
-]
--- hw_hifi/model/__init__.py
+++ hw_hifi/model/__init__.py
@@ -1,9 +1,7 @@
-from hw_hifi.model.baseline_model import BaselineModel
 from hw_hifi.model.hifi_gan import HiFiGAN
 from hw_hifi.model.true_hifi_gan import TrueHiFiGAN
 
 __all__ = [
-    "BaselineModel",
     "HiFiGAN",
     "TrueHiFiGAN"
 ]
