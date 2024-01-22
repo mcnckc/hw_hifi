fb4c472dea962993c08e11455cb174279c61ffe2
mcnckc <s20b1_gusev@179.ru>
2024-01-22 03:04:59 +0300

    debug

--- hw_hifi/model/wla_hifi_gan.py
+++ hw_hifi/model/wla_hifi_gan.py
@@ -32,6 +32,7 @@ class WLAHiFiGAN(BaseModel):
         self.discr_loss = DiscriminatorLoss()
         self.feature_loss = FeatureLoss()
         self.generator_loss = GeneratorLoss()
+        print("USING WLA MODEL")
 
     def generator_params(self):
         return filter(lambda p: p.requires_grad, self.generator.parameters())
